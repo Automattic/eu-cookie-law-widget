9cb9a74e45f21883cac014beace4ffff40d105d5
Alexander Kirk <alexander@kirk.at>
2015-06-23 13:52:31 +0200

    Settings form now contains all settings, saving settings works, too

--- eu-cookie-law.php
+++ eu-cookie-law.php
@@ -19,15 +19,24 @@ class EU_Cookie_Law_Widget extends WP_Widget {
 			'eu_cookie_law_widget',
 			__( 'EU Cookie Law Banner', 'eucookielaw' ),
 			array(
-				'description' => __( 'Display a banner.', 'eucookielaw' ),
+				'description' => __( 'Display a banner for compliance with the EU Cookie Law.', 'eucookielaw' ),
 			),
 			array()
 		);
 
+		$this->defaults = array(
+			'hide' => 'button',
+			'text' => 'default',
+			'customtext' => '',
+			'policyurl' => 'default',
+			'custompolicyurl' => '',
+		);
 	}
 
 	public function widget( $args, $instance ) {
 		$instance = wp_parse_args( $instance, $this->defaults );
+		echo $args['before_widget'];
+		echo $args['after_widget'];
 		add_action( 'wp_footer', array( $this, 'footer' ) );
 	}
 
@@ -48,19 +57,85 @@ FOOTER;
 
 	public function form( $instance ) {
 		$instance = wp_parse_args( $instance, $this->defaults );
-		echo <<<SETTINGS
-<label>Banner text</label><br /><textarea name="text"></textarea><br />
-<br />
-Hide the cookie banner<br />
-<label><input type="radio" name="hide" value="button" checked="checked" /> after the user clicks the dismiss button</label><br />
-<label><input type="radio" name="hide" value="scroll" /> after the user scrolls the page</label><br />
-<label><input type="radio" name="hide" value="time" /> after this time: <input type="text" name="hidetime" /></label><br />
+		?>
+<p>
+	<strong><?php _e( 'Hide the cookie banner' ); ?></strong>
+	<ul>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'hide' ); ?>" value="button" <?php checked( $instance['hide'], 'button' ); ?> /> <?php _e( 'after the user clicks the dismiss button' ); ?></label>
+		</li>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'hide' ); ?>" value="scroll" <?php checked( $instance['hide'], 'scroll' ); ?> /> <?php _e( 'after the user scrolls the page' ); ?></label><br />
+		</li>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'hide' ); ?>" value="time" <?php checked( $instance['hide'], 'time' ); ?> /> <?php _e( 'after this amount of time:' ); ?></label> <input type="number" name="<?php echo $this->get_field_name( 'hidetime' ); ?>" value="<?php echo esc_attr( $instance['hidetime'] ); ?>" style="width: 3em" /> <?php _e( 'seconds' ); ?>
+		</li>
+	</ul>
+</p>
+<hr />
+<p>
+	<strong><?php _e( 'Banner text' ); ?></strong>
+	<ul>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'text' ); ?>" value="default" <?php checked( $instance['text'], 'default' ); ?> /> <?php _e( 'Default' ); ?></label>
+		</li>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'text' ); ?>" value="custom" <?php checked( $instance['text'], 'custom' ); ?> /> <?php _e( 'Custom:' ); ?></label><br/>
+		</li>
+	</ul>
+	<textarea class="widefat" name="<?php echo $this->get_field_name( 'customtext' ); ?>"><?php echo esc_html( $instance['customtext'] ); ?></textarea>
+</p>
+<hr />
+<p>
+	<strong><?php _e( 'Policy URL' ); ?></strong>
+	<ul>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'policyurl' ); ?>" value="default" <?php checked( $instance['policyurl'], 'default' ); ?> /> <?php _e( 'Default' ); ?></label>
+		</li>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'policyurl' ); ?>" value="custom" <?php checked( $instance['policyurl'], 'custom' ); ?> /> <?php _e( 'Custom:' ); ?></label> <input type="text" class="widefat" name="<?php echo $this->get_field_name( 'custompolicyurl' ); ?>" placeholder="http://" style="margin-top: .5em" value="<?php echo esc_attr( $instance['custompolicyurl'] ); ?>" /><br/>
+		</li>
+	</ul>
 
-SETTINGS;
+</p>
+
+		<?php
 	}
 
-	public function update( $new_instance ) {
+	public function update( $new_instance, $old_instance ) {
 		$instance = array();
+
+		if ( in_array( $new_instance['hide'], array( 'button', 'scroll', 'time' ) ) ) {
+			$instance['hide'] = $new_instance['hide'];
+		}
+
+		if ( isset( $new_instance['hidetime'] ) ) {
+			$instance['hidetime'] = min( 1000, max( 5, intval( $new_instance['hidetime'] ) ) );
+		}
+
+		if ( in_array( $new_instance['text'], array( 'default', 'custom' ) ) ) {
+			$instance['text'] = $new_instance['text'];
+		}
+
+		if ( isset( $new_instance['customtext'] ) ) {
+			$instance['customtext'] = mb_substr( $new_instance['customtext'], 0, 4096 );
+		} else {
+			$instance['text'] = 'default';
+		}
+
+		if ( in_array( $new_instance['policyurl'], array( 'default', 'custom' ) ) ) {
+			$instance['policyurl'] = $new_instance['policyurl'];
+		}
+
+		if ( isset( $new_instance['custompolicyurl'] ) ) {
+			$instance['custompolicyurl'] = esc_url( $new_instance['custompolicyurl'], array( 'http', 'https' ) );
+
+			if ( strlen( $instance['custompolicyurl'] ) < 10 ) {
+				$instance['policyurl'] = 'default';
+			}
+		} else {
+			$instance['policyurl'] = 'default';
+		}
 
 		return $instance;
 	}

487a996200db3abee06dcea712bb29e1141859e2
Alexander Kirk <alexander@kirk.at>
2015-06-23 16:01:10 +0200

    Move form HTML into a template, too

--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,4 @@
-<?php
-$blog_url = get_bloginfo( 'url' );
-$instance = $this->instance;
-
-?><div id="eu-cookie-law" style="position: fixed; bottom: 0; left: 0; right: 0; height: 8em; background-color: #fff; padding: 2em">
+<div id="eu-cookie-law" style="position: fixed; bottom: 0; left: 0; right: 0; height: 8em; background-color: #fff; padding: 2em">
 	<form action="<?php echo $blog_url ?>" method="post">
 		<div style="margin-right: 30%">
 			<?php if ( $instance['text'] == 'default' || empty( $instance['customtext'] ) ) {
---
+++ templates/form.php
@@ -0,0 +1,49 @@
+<p>
+	<strong><?php _e( 'Hide the cookie banner' ); ?></strong>
+	<ul>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'hide' ); ?>" value="button" <?php checked( $instance['hide'], 'button' ); ?> /> <?php _e( 'after the user clicks the dismiss button' ); ?></label>
+		</li>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'hide' ); ?>" value="scroll" <?php checked( $instance['hide'], 'scroll' ); ?> /> <?php _e( 'after the user scrolls the page' ); ?></label>
+		</li>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'hide' ); ?>" value="time" <?php checked( $instance['hide'], 'time' ); ?> /> <?php _e( 'after this amount of time:' ); ?></label> <input type="number" name="<?php echo $this->get_field_name( 'hidetime' ); ?>" value="<?php echo esc_attr( $instance['hidetime'] ); ?>" style="width: 3em" /> <?php _e( 'seconds' ); ?>
+		</li>
+	</ul>
+</p>
+<hr />
+<p>
+	<strong><?php _e( 'Banner text' ); ?></strong>
+	<ul>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'text' ); ?>" value="default" <?php checked( $instance['text'], 'default' ); ?> /> <?php _e( 'Default' ); ?></label>
+		</li>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'text' ); ?>" value="custom" <?php checked( $instance['text'], 'custom' ); ?> /> <?php _e( 'Custom:' ); ?></label>
+		</li>
+	</ul>
+	<textarea class="widefat" name="<?php echo $this->get_field_name( 'customtext' ); ?>"><?php echo esc_html( $instance['customtext'] ); ?></textarea>
+</p>
+<hr />
+<p>
+	<strong><?php _e( 'Policy URL' ); ?></strong>
+	<ul>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'policyurl' ); ?>" value="default" <?php checked( $instance['policyurl'], 'default' ); ?> /> <?php _e( 'Default' ); ?></label>
+		</li>
+		<li>
+			<label><input type="radio" name="<?php echo $this->get_field_name( 'policyurl' ); ?>" value="custom" <?php checked( $instance['policyurl'], 'custom' ); ?> /> <?php _e( 'Custom:' ); ?></label> <input type="text" class="widefat" name="<?php echo $this->get_field_name( 'custompolicyurl' ); ?>" placeholder="http://" style="margin-top: .5em" value="<?php echo esc_attr( $instance['custompolicyurl'] ); ?>" />
+		</li>
+	</ul>
+</p>
+<p>
+	<strong><?php _e( 'Policy Link Text' ); ?></strong>
+	<label><input type="text" class="widefat" name="<?php echo $this->get_field_name( 'policylinktext' ); ?>"value="<?php echo esc_attr( $instance['policylinktext'] ); ?>" />
+<hr />
+<p>
+	<strong><?php _e( 'Button text' ); ?></strong>
+	<label><input type="text" class="widefat" name="<?php echo $this->get_field_name( 'button' ); ?>"value="<?php echo esc_attr( $instance['button'] ); ?>" /></label>
+</p>
+<hr />
+<p class="small"><?php _e( 'It is your own responsibility to ensure your site complies with the relevant laws.' ); ?> <a href="https://en.support.wordpress.com/cookies-widget"><?php _e( 'Click here for more information' ); ?></a></p>
